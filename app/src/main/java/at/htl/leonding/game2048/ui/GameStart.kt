e0af922a085649bb3b1ff0b274bb2ffce4cadbd3
MctomSpdo <mctom.spdo@gmail.com>
2024-01-13 11:21:07 +0100

    GameStart screen design

--- app/src/main/java/at/htl/leonding/game2048/ui/GameStart.kt
+++ app/src/main/java/at/htl/leonding/game2048/ui/GameStart.kt
@@ -1,23 +1,81 @@
 package at.htl.leonding.game2048.ui
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldColors
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import at.htl.leonding.game2048.ui.theme.GameBackground
 import at.htl.leonding.game2048.viewmodel.GameViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun StartScreen(viewModel: GameViewModel) {
-    Column {
-        TextField(value = viewModel.name.value, onValueChange = {value -> viewModel.name.value = value})
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .background(GameBackground),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Text(
+            text = "2048",
+            color = Color.White,
+            fontSize = 25.sp
+        )
+        TextField(
+            modifier = Modifier.padding(35.dp),
+            value = viewModel.name.value,
+            onValueChange = {value -> viewModel.name.value = value},
+            colors = TextFieldDefaults.textFieldColors(
+                textColor = Color.White,
+                containerColor = GameBackground
+            ),
+            label = {
+                Text(
+                    text = "Name"
+                )
+            }
+        )
         Button(
             onClick = { viewModel.startGame() },
-            enabled = viewModel.name.value.isNotEmpty()
+            enabled = viewModel.name.value.isNotEmpty(),
+            colors = ButtonDefaults.buttonColors(
+                disabledContainerColor = Color.DarkGray,
+                containerColor = Color.White
+            )
         ) {
-            Text(text = "submit")
+            if(viewModel.name.value.isNotEmpty()) {
+                Text(
+                    text = "submit",
+                    color = Color.Black
+                )
+            } else {
+                Text(
+                    text = "submit",
+                    color = Color.White
+                )
+            }
         }
     }
+}
+
+@Preview
+@Composable
+fun StartScreenPreview() {
+    StartScreen(viewModel = GameViewModel())
 }
--- app/src/main/java/at/htl/leonding/game2048/ui/theme/Color.kt
+++ app/src/main/java/at/htl/leonding/game2048/ui/theme/Color.kt
@@ -10,7 +10,7 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
-val GameBackground = Color(0xffbbada0)
+val GameBackground = Color(0xFF615A53)
 val GameCellBackground = Color(238f/255, 228f/255, 218f/255, 0.35f)
 
 val Game2Color = Color(0xFF776e65)
